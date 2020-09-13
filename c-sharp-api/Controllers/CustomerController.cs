570d29f896b5d08e49e1a5331c77cb1ffe710f17
John Adamchak <johnray987@yahoo.com>
2020-09-12 19:36:17 -0500

    Initial commit

---
+++ c-sharp-api/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net;
+
+namespace customer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Object Get()
+        {
+            var response = new {
+                CustomerName = "John Doe",
+                CustomerId = "012321231"
+            };
+
+            return StatusCode((int)HttpStatusCode.OK, response);
+        }
+    }
+
+}
---
+++ c-sharp-api/Customer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace customer
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+
+        public string CustomerId { get; set; }
+    }
+
+}
